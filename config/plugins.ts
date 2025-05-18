4b37a55e2bb637e8308f21503c34e601d97c5c67
thewaleedtariq <waleedsulehry007@gmail.com>
2025-05-19 02:30:26 +0500

    first commit

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,19 @@
-export default () => ({});
+module.exports = {
+    seo: {
+        enabled: true,
+    },
+    'strapi-5-sitemap-plugin': {
+        enabled: true,
+        config: {
+            // Optional but recommended
+            autoGenerate: true,
+            caching: true,
+            maxEntriesPerSitemap: 5000,
+            includeDrafts: false,
+            // Optional: base URL of your frontend
+            hostname: 'http://localhost:3000',
+
+            // exclude: ['/admin'], // exclude paths if needed
+        },
+    },
+};
---
+++ types/generated/components.d.ts
@@ -0,0 +1,99 @@
+import type { Schema, Struct } from '@strapi/strapi';
+
+export interface ProductSeoMetadata extends Struct.ComponentSchema {
+  collectionName: 'components_product_seo_metadata';
+  info: {
+    description: '';
+    displayName: 'seo-metadata';
+    icon: 'chartCircle';
+  };
+  attributes: {
+    keywords: Schema.Attribute.String;
+    metaDescription: Schema.Attribute.String;
+    metaImage: Schema.Attribute.Media<
+      'images' | 'files' | 'videos' | 'audios',
+      true
+    >;
+    metaTitle: Schema.Attribute.String;
+    openGraphDescription: Schema.Attribute.String;
+    openGraphImage: Schema.Attribute.Media<
+      'images' | 'files' | 'videos' | 'audios',
+      true
+    >;
+    openGraphTitle: Schema.Attribute.String;
+  };
+}
+
+export interface ProductSize extends Struct.ComponentSchema {
+  collectionName: 'components_product_sizes';
+  info: {
+    description: '';
+    displayName: 'size';
+    icon: 'attachment';
+  };
+  attributes: {
+    sizess: Schema.Attribute.Enumeration<['Small', 'Medium', 'Large']>;
+  };
+}
+
+export interface SharedOpenGraph extends Struct.ComponentSchema {
+  collectionName: 'components_shared_open_graphs';
+  info: {
+    displayName: 'openGraph';
+    icon: 'project-diagram';
+  };
+  attributes: {
+    ogDescription: Schema.Attribute.String &
+      Schema.Attribute.Required &
+      Schema.Attribute.SetMinMaxLength<{
+        maxLength: 200;
+      }>;
+    ogImage: Schema.Attribute.Media<'images'>;
+    ogTitle: Schema.Attribute.String &
+      Schema.Attribute.Required &
+      Schema.Attribute.SetMinMaxLength<{
+        maxLength: 70;
+      }>;
+    ogType: Schema.Attribute.String;
+    ogUrl: Schema.Attribute.String;
+  };
+}
+
+export interface SharedSeo extends Struct.ComponentSchema {
+  collectionName: 'components_shared_seos';
+  info: {
+    displayName: 'seo';
+    icon: 'search';
+  };
+  attributes: {
+    canonicalURL: Schema.Attribute.String;
+    keywords: Schema.Attribute.Text;
+    metaDescription: Schema.Attribute.String &
+      Schema.Attribute.Required &
+      Schema.Attribute.SetMinMaxLength<{
+        maxLength: 160;
+        minLength: 50;
+      }>;
+    metaImage: Schema.Attribute.Media<'images'>;
+    metaRobots: Schema.Attribute.String;
+    metaTitle: Schema.Attribute.String &
+      Schema.Attribute.Required &
+      Schema.Attribute.SetMinMaxLength<{
+        maxLength: 60;
+      }>;
+    metaViewport: Schema.Attribute.String;
+    openGraph: Schema.Attribute.Component<'shared.open-graph', false>;
+    structuredData: Schema.Attribute.JSON;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'product.seo-metadata': ProductSeoMetadata;
+      'product.size': ProductSize;
+      'shared.open-graph': SharedOpenGraph;
+      'shared.seo': SharedSeo;
+    }
+  }
+}
